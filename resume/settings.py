5d9d08c3281f3041538d52b7d8cf67d80d394045
Корякин Айсен <koryakinbezlimit@gmail.com>
2024-10-09 23:00:02 +0300

    change setting

--- resume/settings.py
+++ resume/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'default-secret-key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deepra_test_task.com']
 
 
 # Application definition
